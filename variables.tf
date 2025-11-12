b80439f6dfd2b0d11410026788e415dd19fd3df3
iamrmad1 <91768407+iamrmad1@users.noreply.github.com>
2025-11-12 15:21:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,12 +16,12 @@ variable "client_secret" {
 
 variable "policy_name" {
   type        = string
+  description = "Display name for the Windows 10 compliance policy"
   default     = "Windows 10 Compliance Policy"
-  description = "Display name"
 }
 
 variable "pilot_group_display_name" {
   type        = string
-  default     = "Intune - Windows - Pilot"
-  description = "Existing Entra ID group to assign policy to"
+  description = "Existing Entra ID group display name to assign the policy to (optional)"
+  default     = ""
 }
