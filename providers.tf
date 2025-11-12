fe53d43cb52894dc72885eca076c37e91c0bc31b
iamrmad1 <91768407+iamrmad1@users.noreply.github.com>
2025-11-12 13:37:27 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    microsoft365wp = {
+      source = "terraprovider/microsoft365wp"
+      version = "0.17.1"
+    }
+  }
+}
+
+provider "microsoft365wp" {
+  # Configuration options
+}
