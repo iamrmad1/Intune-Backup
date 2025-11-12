a7cac93964ff736024e808d93b8780e84b78a940
iamrmad1 <91768407+iamrmad1@users.noreply.github.com>
2025-11-12 14:31:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,34 +2,36 @@ resource "microsoft365wp_device_compliance_policy" "win10" {
   display_name = var.policy_name
   description  = "Windows 10 compliance baseline"
 
-  // Required block: scheduled actions
-  scheduled_actions_for_rule {
-    rule_name = "PasswordRequired"
-
-    scheduled_action_configurations {
-      action_type = "block"
-      grace_period_hours = 0
+  // Required argument: scheduled_actions_for_rule
+  scheduled_actions_for_rule = [
+    {
+      rule_name = "PasswordRequired"
+      scheduled_action_configurations = [
+        {
+          action_type        = "block"
+          grace_period_hours = 0
+        }
+      ]
     }
-  }
+  ]
 
-  // Platform-specific settings go inside settings blocks
-  settings {
-    setting_name  = "passwordRequired"
-    setting_value = "true"
-  }
-
-  settings {
-    setting_name  = "passwordMinimumLength"
-    setting_value = "8"
-  }
-
-  settings {
-    setting_name  = "passwordRequiredType"
-    setting_value = "alphanumeric"
-  }
-
-  settings {
-    setting_name  = "osMinimumVersion"
-    setting_value = "10.0.19045.0"
-  }
+  // Compliance settings are passed as JSON-style maps
+  settings = [
+    {
+      setting_name  = "passwordRequired"
+      setting_value = "true"
+    },
+    {
+      setting_name  = "passwordMinimumLength"
+      setting_value = "8"
+    },
+    {
+      setting_name  = "passwordRequiredType"
+      setting_value = "alphanumeric"
+    },
+    {
+      setting_name  = "osMinimumVersion"
+      setting_value = "10.0.19045.0"
+    }
+  ]
 }
