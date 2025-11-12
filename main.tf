7634001265e6de3ea507c3b9faff4fee672de70d
iamrmad1 <91768407+iamrmad1@users.noreply.github.com>
2025-11-12 14:26:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,4 @@
 // main.tf
-
-// 1) Create an Entra ID security group for device targeting
-resource "microsoft365wp_group" "win_pilot" {
-  display_name     = var.device_group_name
-  description      = "Pilot group for Windows device configuration"
-  mail_nickname    = "win_pilot_group"
-  security_enabled = true
-  mail_enabled     = false
-}
-
 // 2) Create a Windows device configuration policy (example CSP)
 resource "microsoft365wp_configuration_policy" "win_disable_consumer" {
   name        = "WIN - Disable Consumer Features"
@@ -25,13 +15,4 @@ resource "microsoft365wp_configuration_policy" "win_disable_consumer" {
       value     = "0"
     }
   ]
-}
-
-// 3) Assign the policy to the group
-resource "microsoft365wp_configuration_policy_assignment" "win_disable_consumer_assign" {
-  policy_id = microsoft365wp_configuration_policy.win_disable_consumer.id
-  target {
-    type     = "group"
-    group_id = microsoft365wp_group.win_pilot.id
-  }
 }
