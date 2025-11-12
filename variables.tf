8cd2f1968de21f046994502befdd9d398da13610
iamrmad1 <91768407+iamrmad1@users.noreply.github.com>
2025-11-12 13:37:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,10 @@
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+// variables.tf
+variable "tenant_id"      { type = string }
+variable "client_id"      { type = string }
+variable "client_secret"  { type = string, sensitive = true }
+
+variable "device_group_name" {
+  type        = string
+  description = "Target Entra ID group for device assignments"
+  default     = "Intune - Windows - Pilot"
+}
