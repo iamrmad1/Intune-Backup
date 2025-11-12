e7508df83617ec8639e707fe73f6c1ba13e35343
iamrmad1 <91768407+iamrmad1@users.noreply.github.com>
2025-11-12 14:18:21 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,12 +1,9 @@
 // providers.tf
-
 terraform {
-  required_version = ">= 0.17.1"
-
   required_providers {
     microsoft365wp = {
       source  = "terraprovider/microsoft365wp"
-      version = "~> 0.1"
+      version = "~> 0.17.1"
     }
   }
 }
@@ -15,7 +12,4 @@ provider "microsoft365wp" {
   tenant_id     = var.tenant_id
   client_id     = var.client_id
   client_secret = var.client_secret
-  # If you prefer certificate auth, replace client_secret with:
-  # client_certificate          = var.client_certificate
-  # client_certificate_password = var.client_certificate_password
 }
